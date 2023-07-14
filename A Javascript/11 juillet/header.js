2eb95fd27a31f0ebf54392b89128abd110a953ba
Yesfaris <yesfaris4524@gmail.com>
2023-07-14 18:23:04 +0100

    weekend-vuejs

---
+++ A Javascript/11 juillet/header.js
@@ -0,0 +1,52 @@
+export default{
+     template: /*html*/
+    ` <header class="header">
+    <div>LOGO</div>
+    <ul>
+        <li> <a href="#">Acceuil </a> </li>
+        <li> <a href="#">Gallerie </a> </li>
+        <li> <a href="#">Contact </a> </li>
+        <li> <a href="#">A propos</a>  </li>
+    </ul>
+    </header>
+    <main>
+    <h1> Ensemble d'articles</h1>
+    <article>
+        <h2>Titre</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus fugit sapiente sunt id nam quam consequuntur consectetur vero, asperiores obcaecati repellat reprehenderit suscipit ducimus ad.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus fugit sapiente sunt id nam quam consequuntur consectetur vero, asperiores obcaecati repellat reprehenderit suscipit ducimus ad.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus fugit sapiente sunt id nam quam consequuntur consectetur vero, asperiores obcaecati repellat reprehenderit suscipit ducimus ad.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus fugit sapiente sunt id nam quam consequuntur consectetur vero, asperiores obcaecati repellat reprehenderit suscipit ducimus ad.</p>
+    </article>
+    <article>
+        <h2>Titre</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus fugit sapiente sunt id nam quam consequuntur consectetur vero, asperiores obcaecati repellat reprehenderit suscipit ducimus ad.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus fugit sapiente sunt id nam quam consequuntur consectetur vero, asperiores obcaecati repellat reprehenderit suscipit ducimus ad.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus fugit sapiente sunt id nam quam consequuntur consectetur vero, asperiores obcaecati repellat reprehenderit suscipit ducimus ad.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus fugit sapiente sunt id nam quam consequuntur consectetur vero, asperiores obcaecati repellat reprehenderit suscipit ducimus ad.</p>
+    </article>
+    <article>
+        <h2>Titre</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus fugit sapiente sunt id nam quam consequuntur consectetur vero, asperiores obcaecati repellat reprehenderit suscipit ducimus ad.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus fugit sapiente sunt id nam quam consequuntur consectetur vero, asperiores obcaecati repellat reprehenderit suscipit ducimus ad.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus fugit sapiente sunt id nam quam consequuntur consectetur vero, asperiores obcaecati repellat reprehenderit suscipit ducimus ad.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus fugit sapiente sunt id nam quam consequuntur consectetur vero, asperiores obcaecati repellat reprehenderit suscipit ducimus ad.</p>
+    </article>
+    <article>
+        <h2>Titre</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus fugit sapiente sunt id nam quam consequuntur consectetur vero, asperiores obcaecati repellat reprehenderit suscipit ducimus ad.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus fugit sapiente sunt id nam quam consequuntur consectetur vero, asperiores obcaecati repellat reprehenderit suscipit ducimus ad.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus fugit sapiente sunt id nam quam consequuntur consectetur vero, asperiores obcaecati repellat reprehenderit suscipit ducimus ad.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus fugit sapiente sunt id nam quam consequuntur consectetur vero, asperiores obcaecati repellat reprehenderit suscipit ducimus ad.</p>
+    </article>
+</main>
+<footer class="footer">
+<ul>
+    <li> <a href="#">Acceuil </a> </li>
+    <li> <a href="#">Gallerie </a> </li>
+    <li> <a href="#">Contact </a> </li>
+    <li> <a href="#">A propos</a>  </li>
+</ul> `
+}
+
+
